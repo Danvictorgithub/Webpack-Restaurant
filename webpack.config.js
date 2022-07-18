8945e416cc2fec0d2b2b3a63484d88c3c678a371
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-07-18 10:27:28 +0800

    Created JS DOM Generate for each nav buttons

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clear:'true',
+  },
+  optimization: {
+    minimizer: [
+      optimization.minimize = true,
+      new CssMinimizerPlugin(),
+    ],
   },
 };
